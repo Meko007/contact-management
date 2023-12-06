cb0c076f3c5b3ef84f0956cffc26df0e4cc61875
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-06 02:09:03 +0100

    slight changes

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -24,13 +24,3 @@ export const isEmail = email => {
     let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return regex.test(email);
 };
-
-export const restrict = role => {
-    return (req, res, next) => {
-        if(req.user.role !== role){
-            res.status(403);
-            throw new Error(`Access denied: User with the ${role} role can't access this resource`);
-        }
-        next();
-    }
-}
