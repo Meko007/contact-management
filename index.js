de94dbcef433f4a15fc593b030d4ae1c4434b8ce
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-10 22:22:40 +0100

    cors addition

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import contactRoutes from './src/routes/contactRoutes.js';
@@ -11,6 +12,7 @@ connectDB();
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/contacts", contactRoutes);
