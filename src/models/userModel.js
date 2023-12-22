10f9b24b52ee79b4e10dfb6c0a7d880c384095fe
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-22 20:57:46 +0100

    new mongoose schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import { isEmail } from '../middleware/validator.js';
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
